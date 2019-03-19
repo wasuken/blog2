cc8912ab11fdc08942ec2ea5cb7c32f152db3b00
wasu <wasu@os3-369-17744.vs.sakura.ne.jp>
2019-03-19 11:27:57 +0900

    blog_controller修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,6 @@
 class CommentsController < ApplicationController
   def create()
-    @id = 1
-    if Comment.maximum(:id).nil?
-      @id = Comment.maximum(:id)
-    end
-    Comment.create(id: @id,
-                   username: params["comment"]["username"],
+    Comment.create(username: params["comment"]["username"],
                    comment: params["comment"]["comment"],
                    blog_id: params["comment"]["blog_id"])
   end
