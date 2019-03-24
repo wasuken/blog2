d3948cb775912a9328029509e4a2d55ed6bfc394
wasuken <wevorence@gmail.com>
2019-03-24 15:36:35 +0900

    changed layout.

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,6 +7,8 @@ class BlogsController < ApplicationController
     @blog_all = Blog.order("created_at ASC")
     @blog_all = Blog.order("id DESC")
     @blog_img_path = view_context.image_path('blog.jpg')
+    @profile_path = view_context.image_path('ninja.png')
+    @title = "重荷怠惰iary"
     render "blogs/index"
   end
   def page
@@ -14,6 +16,7 @@ class BlogsController < ApplicationController
     @comments = Comment.where("blog_id = ?", @id).all
     @comment = Comment.new
     @blog = Blog.where(id: @id).first
+    @title = @blog.title
     render "blogs/page"
   end
   def index_json
@@ -23,6 +26,7 @@ class BlogsController < ApplicationController
     render json: @blog
   end
   def tags
+    @title = "重荷怠惰iary"
     # 登場回数順で登場回数付きのタグのJSONを返す。
     @all_tags = Blog.select(:tags_string).map{|v| v["tags_string"].split(' ')}
                   .flatten
@@ -33,6 +37,7 @@ class BlogsController < ApplicationController
              .map{|v| {size: @all_tags.count(v), string: v}}
   end
   def tags_page
+    @title = "重荷怠惰iary"
     @tags = Blog.select(:tags_string).map{|v| v["tags_string"].split(' ')}
               .flatten
     @tags = @tags
@@ -42,6 +47,7 @@ class BlogsController < ApplicationController
     render "blogs/tags"
   end
   def tag_page
+    @title = "重荷怠惰iary"
     @blogs = Blog.where("tags_string like ?", "%#{params[:tag]}%")
     render "blogs/tag"
   end
